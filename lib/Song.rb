a661a3ea60f7232b3043e40fdedf7b3894fe08f3
elsa chen <snowing222@gmail.com>
2020-06-07 21:10:58 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -12,25 +12,25 @@ class Song
     self.genre=genre if genre
   end
 
-  def artist=(artist)
+  def artist=(artist) #Special artist attr setter
     @artist=artist
     artist.add_song(self)
   end
 
-  def genre=(genre)
+  def genre=(genre) #Special genre att setter
     @genre=genre
     genre.songs<<self unless genre.songs.include?(self)
   end
 
-  def self.all
+  def self.all #Class reader
     @@all
   end
 
-  def self.create(name)
+  def self.create(name) #Custom constructor
     new(name).tap {|o|o.save}
   end
 
-  def self.new_from_filename(file_name)
+  def self.new_from_filename(file_name) #Custom constructor
     song_name=file_name.split(" - ")[1]
     artist_name=file_name.split(" - ")[0]
     genre_name=file_name.split(" - ")[2].gsub(".mp3","")
@@ -40,7 +40,7 @@ class Song
     Song.new(song_name,artist,genre)
   end
 
-  def self.create_from_filename(name)
+  def self.create_from_filename(name) #Custom constructor
     new_from_filename(name).tap{|o|o.save}
   end
 
