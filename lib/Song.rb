1c8653a44228746870d2a25d4ed40718c065f01e
elsa chen <snowing222@gmail.com>
2020-06-07 21:40:55 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -31,7 +31,7 @@ class Song
   end
 
   def self.new_from_filename(file_name) #Custom constructor
-    file_name.gsub(".mp3","")
+    file_name.tap {|o|o.gsub(".mp3","")}
     artist_name=file_name.split(" - ")[0]
     song_name=file_name.split(" - ")[1]
     genre_name=file_name.split(" - ")[2]
